a72201fa4ef9b786ec5257a8abf8963a21e2e127
josh <josh@myfair.co>
2022-03-07 22:12:15 +0900

    LinkedList

--- src/prac/LinkedListPrac.java
+++ src/prac/LinkedListPrac.java
@@ -1,6 +1,14 @@
 package prac;
 
+import java.util.LinkedList;
+
 //prec
 public class LinkedListPrac {
-
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("one");
+        linkedList.add("two");
+        linkedList.add(1, "three");
+        System.out.println(linkedList);
+    }
 }
